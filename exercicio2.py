23096083a49f46ef7d958364d65f0f965ee59921
Alexandre-cod <81189991+Alexandre-cod@users.noreply.github.com>
2021-04-27 18:14:01 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -17,9 +17,34 @@ def cria_baralho():
     j=2
     lista.append('A{}'.format(copas))
     while i<=10:
-        lista.append('{}''{}'.format(i,copas))
+        lista.append('{}''{}'.format(j,copas))
         j+=1
     lista.append('J{}'.format(copas))
     lista.append('Q{}'.format(copas))
     lista.append('K{}'.format(copas))
     
+
+
+    ouros = '♦'
+    k=2
+    lista.append('A{}'.format(ouros))
+    while i<=10:
+        lista.append('{}''{}'.format(k,ouros))
+        k+=1
+    lista.append('J{}'.format(ouros))
+    lista.append('Q{}'.format(ouros))
+    lista.append('K{}'.format(ouros))
+    
+
+    paus = '♣'
+    m=2
+    lista.append('A{}'.format(paus))
+    while i<=10:
+        lista.append('{}''{}'.format(m,paus))
+        m+=1
+    lista.append('J{}'.format(paus))
+    lista.append('Q{}'.format(paus))
+    lista.append('K{}'.format(paus))
+
+
+    return lista
