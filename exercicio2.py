1ef8c758b68b4711326a9e13a332a2083bdc0d6c
Alexandre-cod <81189991+Alexandre-cod@users.noreply.github.com>
2021-04-27 18:09:40 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,13 +1,25 @@
 def cria_baralho():
     lista = []
     
+    espadas = '♠'
     i=2
     lista.append('A{}'.format(espadas))
     while i<=10:
-        espadas = '♠'
         lista.append('{}''{}'.format(i,espadas))
         i+=1
     lista.append('J{}'.format(espadas))
     lista.append('Q{}'.format(espadas))
     lista.append('K{}'.format(espadas))
+
+
+
+    copas = '♥'
+    j=2
+    lista.append('A{}'.format(copas))
+    while i<=10:
+        lista.append('{}''{}'.format(i,copas))
+        j+=1
+    lista.append('J{}'.format(copas))
+    lista.append('Q{}'.format(copas))
+    lista.append('K{}'.format(copas))
     
