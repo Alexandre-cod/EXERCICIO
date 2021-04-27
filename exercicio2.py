a310813db17851ba0910021bef11c341989b450e
Alexandre-cod <81189991+Alexandre-cod@users.noreply.github.com>
2021-04-27 18:08:59 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,2 +1,13 @@
-def funcao(a,n):
-  return a
+def cria_baralho():
+    lista = []
+    
+    i=2
+    lista.append('A{}'.format(espadas))
+    while i<=10:
+        espadas = '♠'
+        lista.append('{}''{}'.format(i,espadas))
+        i+=1
+    lista.append('J{}'.format(espadas))
+    lista.append('Q{}'.format(espadas))
+    lista.append('K{}'.format(espadas))
+    
